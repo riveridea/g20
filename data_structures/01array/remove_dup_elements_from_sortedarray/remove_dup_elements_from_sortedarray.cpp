84eedf320a1becde7b8c9cb8b685fd94eb811d31
liangyu <yuliang.name@gmail.com>
2016-08-15 23:50:56 -0400

    removed duplicate from sorted array in python

---
+++ data_structures/01array/remove_dup_elements_from_sortedarray/remove_dup_elements_from_sortedarray.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+	/*
+
+	Remove Duplicates from Sorted Array:
+	https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+	
+Given a sorted array, remove the duplicates in place such that 
+each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this 
+in place with constant memory.
+
+For example,
+Given input array A = [1,1,2],
+
+Your function should return length = 2, and A is now [1,2] respectively.
+It doesn't matter what you leave beyond the new length..
+*/
+    int removeDuplicates1(vector<int>& nums) {
+        int n = nums.size();
+        
+        if (n <= 1) {
+            return n;
+        }
+        
+        int index = 0;
+        int start = 1;
+        
+        while (start < n) {
+            if (nums[start] == nums[index]) {
+                start++;
+            } else {
+                index++;
+                nums[index] = nums[start];
+            }
+        }
+        
+        int count = index +1;
+        
+        while (index < n) {
+            nums[index] = 0;
+            index++;
+        }
+        
+        return count;
+    }
+
+
+    /*
+Remove Duplicates from Sorted Array II:
+https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+
+Follow up for "Remove Duplicates":
+What if duplicates are allowed at most twice?
+
+For example,
+Given sorted array nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first 
+five elements of nums being 1, 1, 2, 2 and 3. It doesn't
+matter what you leave beyond the new length.
+*/
+
+// 之前 leetcode上 跑通过了的，为什么改了一下就不过了
+//http://blog.csdn.net/fightforyourdream/article/details/12883543
+    int removeDuplicates(vector<int>& nums) {
+        // index用来遍历数组，end保存新数组的最后位置
+        int length = nums.size();
+        int end = 0;
+        int count = 0;
+        
+        if (length < 3) {
+            return length;
+        }
+        
+        for (int index = 1; index < length; index++) {
+            // 重复的情况
+            if (nums[end] == nums[index]) {
+                // 当且仅当重复一次（共出现两次）的情况下才复制
+                if (count == 0) {
+                    nums[++end] = nums[index];
+                } else {
+                    count++;
+                }
+            } else {
+                // 不重复的情况，更新end的位置，然后直接复制过去 
+                nums[++end] = nums[index];
+                // 重置counter
+                count = 0;
+            }
+        }
+        
+        return end+1;
+    }
+
+
+};
+
+int main() {
+	vector<int> nums = {1, 1};
+	Solution s;
+	cout << "{1, 1} result is: " <<  s.removeDuplicates1(nums) << endl;
+	return 0;
+}
