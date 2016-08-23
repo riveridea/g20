c04387697e2303f20e2e7f5e376d1990eafcfd95
liangyu <yuliang.name@gmail.com>
2016-08-23 19:57:15 -0400

    finished intersection of linkedlist

--- data_structures/02list/list_cycle/intersection_of_two_linkedlist.cpp
+++ data_structures/02list/list_cycle/intersection_of_two_linkedlist.cpp
@@ -1,4 +1,6 @@
 /*
+https://leetcode.com/problems/intersection-of-two-linked-lists
+
 Write a program to find the node at which the intersection of two 
 singly linked lists begins.
 
@@ -39,7 +41,47 @@ public:
     the same pace, if meet, then we get intersection pointer.
 	*/
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+    	if (headA == NULL || headB == NULL) {
+    		return NULL;
+    	}
+
+    	ListNode* iter1 = headA;
+    	ListNode* iter2 = headB;
+
+    	int len1 = 1;
+
+    	while (iter1->next != NULL) {
+    		iter1 = iter1->next;
+    		len1++;
+    	}
+
+    	int len2 = 1;
+
+    	while (iter2->next != NULL) {
+    		iter2 = iter2->next;
+    		len2++;
+    	}
+
+    	if (iter1 != iter2) {
+    		return NULL;
+    	}
+
+    	if (len1 > len2) {
+    		for (int i = 0; i < len1-len2; i++) {
+    			headA = headA->next;
+    		}
+    	} else if (len2 > len1){
+            for (int i = 0; i < len2 - len1; i++) {
+                headB = headB->next;
+            }
+    	}
+
+    	while (headA != headB) {
+            headA = headA->next;
+            headB = headB->next;
+        }
         
+        return headA;
     }
 
     // Method 2: smart
