250b1e18792f5abeb8df66520c79fad74c261a78
liangyu <yuliang.name@gmail.com>
2016-08-31 22:32:45 -0400

    find the maximum elem in binary tree with dfs

---
+++ data_structures/05tree/height_size/maximum_elements_in_bt.cpp
@@ -0,0 +1,54 @@
+/*
+Give an algorithm for finding maximum element in binary tree.
+*/
+
+#include <cmath>
+#include <iostream>
+#include <cstddef>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    int FindMax(TreeNode* root) {
+    	int root_val = 0;
+    	int left_val = 0;
+    	int right_val = 0;
+    	int max = INT_MIN;
+
+    	if (root == NULL) {
+            return 0;
+    	} else {
+    		root_val = root->val;
+    		left_val = FindMax(root->left);
+    		right_val = FindMax(root->right);
+
+            if (left_val < right_val) {
+            	max = right_val;
+            } else {
+            	max = left_val;
+            }
+
+            if (root_val > max) {
+            	max = root_val;
+            }
+    	}
+
+    	return max;
+    }
+};
+
+int main(void) {
+    Solution c;
+    TreeNode root = TreeNode(9);
+    cout << c.FindMax(&root) << endl;
+
+	return 0;
+}
