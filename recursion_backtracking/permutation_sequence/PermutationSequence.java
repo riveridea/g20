76fe2f8bed06be097784afbd340c8922b14f40d2
liangyu <yuliang.name@gmail.com>
2016-11-14 00:47:20 -0500

    added missing questions from 1 to 100 second batch

--- recursion_backtracking/permutation_sequence/PermutationSequence.java
+++ recursion_backtracking/permutation_sequence/PermutationSequence.java
@@ -1,4 +1,7 @@
 /*
+60. Permutation Sequence
+https://leetcode.com/problems/permutation-sequence/
+
 The set [1,2,3,…,n] contains a total of n! unique permutations.
 
 By listing and labeling all of the permutations in order,
@@ -15,15 +18,18 @@ Given n and k, return the kth permutation sequence.
 Note: Given n will be between 1 and 9 inclusive.
 */
 
+public class Solution {
+    public String getPermutation(int n, int k) {
+        
+    }
+}
 
+/*
 http://gongxuns.blogspot.com/2012/12/leetcode-permutation-sequence.html
 http://discuss.leetcode.com/questions/237/permutation-sequence
 http://blog.csdn.net/havenoidea/article/details/12837441
 http://blog.unieagle.net/2012/10/16/leetcode%E9%A2%98%E7%9B%AE%EF%BC%9Apermutation-sequence/
 
-
-
-/* java */
 
 public class Solution {
     public String getPermutation(int n, int k) {
@@ -53,3 +59,6 @@ public class Solution {
         return buf.toString();
     }
 }
+
+
+*/
