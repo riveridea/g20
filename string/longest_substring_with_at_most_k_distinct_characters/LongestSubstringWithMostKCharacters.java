7e707cc6224ee30b013a1bad45d399b10a81578c
liangyu <yuliang.name@gmail.com>
2016-11-15 01:17:00 -0500

    sort out all the math-related questions

---
+++ string/longest_substring_with_at_most_k_distinct_characters/LongestSubstringWithMostKCharacters.java
@@ -0,0 +1,10 @@
+/*
+340. Longest Substring with At Most K Distinct Characters
+
+Given a string, find the length of the longest substring T 
+that contains at most k distinct characters. For example, 
+Given s = “eceba” and k = 2, T is "ece" which its length is 3.
+
+http://buttercola.blogspot.com/2016/06/leetcode-340-longest-substring-with-at.html
+http://www.programcreek.com/2013/02/longest-substring-which-contains-2-unique-characters/
+*/
