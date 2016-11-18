d36e8dec9ebce8a0fb24054a977fe724bb32a40f
Yu, Liang <liangyu@users.noreply.github.com>
2016-11-18 11:11:09 -0500

    Create ShortestPalindrome.java

---
+++ string/ShortestPalindrome.java
@@ -0,0 +1,18 @@
+/*
+214. Shortest Palindrome
+https://leetcode.com/problems/shortest-palindrome/
+
+Given a string S, you are allowed to convert it to a palindrome 
+by adding characters in front of it. Find and return the shortest
+palindrome you can find by performing this transformation.
+
+For example:
+Given "aacecaaa", return "aaacecaaa".
+Given "abcd", return "dcbabcd".
+*/
+
+public class Solution {
+    public String shortestPalindrome(String s) {
+        
+    }
+}
