a99b013d1a0836517fae1d99130bec0e29f7bf8b
liangyu <yuliang.name@gmail.com>
2016-11-19 22:06:47 -0500

    added remove invalid paratheses

---
+++ string/longest_substring_with_at_most_two_distinct_characters/LongestSubstringWithMostTwoCharacters.java
@@ -0,0 +1,10 @@
+/*
+159. Longest Substring With Most Two Characters
+
+Given a string S, find the length of the longest substring T 
+that contains at most two distinct characters.
+
+For example, Given S = “eceba”, T is "ece" which its length is 3.
+
+https://tonycao.gitbooks.io/leetcode-locked/content/LeetCode%20Locked/c1.7.html
+*/
---
+++ string/remove_invalid_parentheses/RemoveInvalidParentheses.java
@@ -0,0 +1,24 @@
+/*
+301. Remove Invalid Parentheses
+https://leetcode.com/problems/remove-invalid-parentheses/
+
+Remove the minimum number of invalid parentheses in order to make 
+the input string valid. Return all possible results.
+
+Note: The input string may contain letters other than the 
+parentheses ( and ).
+
+Examples:
+"()())()" -> ["()()()", "(())()"]
+"(a)())()" -> ["(a)()()", "(a())()"]
+")(" -> [""]
+
+Similar Problem:
+Valid Parentheses
+*/
+
+public class Solution {
+    public List<String> removeInvalidParentheses(String s) {
+        
+    }
+}
