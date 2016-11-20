845ec0087ad33ad6d8708dfb7de9ff4b5f83c13c
liangyu <yuliang.name@gmail.com>
2016-11-19 21:38:13 -0500

    modified 2 sum

--- array/majority_element/MajorityElement2.java
+++ array/majority_element/MajorityElement2.java
@@ -2,7 +2,8 @@
 229. Majority Element II
 https://leetcode.com/problems/majority-element-ii/
 
-Given an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
+Given an integer array of size n, find all elements that appear more 
+than ⌊ n/3 ⌋ times. The algorithm should run in linear time and in O(1) space.
 
 Hint:
 
