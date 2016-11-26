7d286fd3c6e9a568c9d6baad50e5fe3b1fb17729
liangyu <yuliang.name@gmail.com>
2016-11-25 23:26:56 -0500

    delelted duplicated questions

--- array/median_of_two_sortedarray/median_of_two_sortedarray.cpp
+++ array/median_of_two_sortedarray/median_of_two_sortedarray.cpp
@@ -2,10 +2,20 @@
 4. Median of Two Sorted Arrays:
 https://leetcode.com/problems/median-of-two-sorted-arrays/
 
-There are two sorted arrays A and B of size m and n
-respectively. Find the median of the two sorted 
-arrays. The overall run time complexity should be
-O(log (m+n)).
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+Find the median of the two sorted arrays. The overall run time complexity 
+should be O(log (m+n)).
+
+Example 1:
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+Example 2:
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
 
 对于一组有限个数的数据来说，它们的中位数是这样的一种数：这群数据
 里的一半的数据比它大，而另外一半数据比它小。 计算有限个数的数据的
@@ -18,3 +28,11 @@ O(log (m+n)).
 //http://www.acmerblog.com/leetcode-median-of-two-sorted-arrays-5330.html
 //http://bangbingsyb.blogspot.com/2014/11/leetcode-median-of-two-sorted-arrays.html
 //http://blog.csdn.net/yysdsyl/article/details/4266515
+
+class Solution {
+public:
+	double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+
+	}
+
+};
