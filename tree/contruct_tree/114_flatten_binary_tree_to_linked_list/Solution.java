05c33d4519715d19442a24f1c28d9349de436673
liangyu <yuliang.name@gmail.com>
2016-12-10 18:31:06 -0500

    updated tree questions

--- tree/contruct_tree/114_flatten_binary_tree_to_linked_list/Solution.java
+++ tree/contruct_tree/114_flatten_binary_tree_to_linked_list/Solution.java
@@ -33,15 +33,76 @@ to the next node of a pre-order traversal.
 
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
  */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
 public class Solution {
-    public void flatten(TreeNode root) {
-        
+
+    // recursively, connecct the right most node of a node to it right node
+    // use tree preorder
+    public TreeNode flatten(TreeNode root) {
+        if (root == null) {
+          return;
+        }  
+
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+
+        root.left = null;
+
+        if (left != null) {
+          root.right = left;
+          root = flatten(left);
+        }
+
+        if (right != null) {
+          root.right = right;
+          root = flatten(right);
+        }
+
+        return root;
     }
+
+    /*
+    * preOrder:
+    * 1) check, if current node is null or leaf, return it.
+    * 2) init a stack to store the right child tree
+    * 3) preOrder travel the tree
+    * 3.1) if current node has left child, store the right child in stack, 
+    *      change the left child to right.  
+    * 3.2) else, get the right child from the stack and 
+    *      set it as the right child as the current node
+    */
+
+    public void flatten2(TreeNode root) {
+        if (root == null || (root.left == null && root.right == null)) {
+            return;
+        }
+
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode current = root;
+
+        while (current != null || !stack.isEmpty()) {
+            while (current.left != null) {
+                if (current.right != null) {
+                    stack.push(current.right);
+                }
+
+                current.right = current.left;
+                current.left = null;
+                current = current.right;
+            }
+
+            if (current.right == null && !stack.isEmpty()) {
+                current.right = stack.pop();
+            }
+        }
+    }
+
+
 }
