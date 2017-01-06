d95b299943a41ad111a1be50fb1fc4a5167edafa
liangyu <yuliang.name@gmail.com>
2017-01-05 23:57:54 -0500

    construct tree from preorder and inorder

--- tree/contruct_tree/105_construct_bt_from_pre_in_order/Solution.java
+++ tree/contruct_tree/105_construct_bt_from_pre_in_order/Solution.java
@@ -12,7 +12,7 @@ Note:You may assume that duplicates do not exist in the tree.
 /**
  * Definition for a binary tree node.
  */
-public class TreeNode {
+class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
@@ -21,6 +21,49 @@ public class TreeNode {
 
 public class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
-        
+        if (preorder.length != inorder.length) {
+        	return null;
+        }
+
+        if (preorder.length == 0) {
+        	return null;
+        }
+
+        int n = preorder.length();
+
+        return buildTreeHelper(preorder, 0, n-1, inorder, 0, n-1);
+    }
+
+    public TreeNode buildTreeHelper(int[] preorder, int sPreorder, int ePreorder
+    	    int[] inorder, int sInorder, int eInorder) {
+        if (sPreorder > ePreorder || sInorder > eInorder) {
+        	return null;
+        }
+
+        TreeNode root = new TreeNode(preorder[sPreorder]);
+        int rootIndex = 0;
+
+        for (int i = rootIndex; i <= eInorder; i++) {
+        	if (inorder[i] == root.val) {
+        		rootIndex = i;
+        		break; // find the root value in inorder array,
+        		// then seperate the array to two parts, each of
+        		// which matches the left or right subtree.
+        	}
+        }
+
+        int leftTreeSize = rootIndex - sInorder;
+        int rightTreeSize = eInorder - rootIndex;
+
+        root.left = buildTreeHelper(preorder, sPreorder+1, sPreorder + rootIndex - sInorder,
+        							inorder, sInorder, rootIndex - 1);
+        root.right = buildTreeHelper(preorder, ePreorder + rootIndex - eInorder + 1, ePreorder,
+        							inorder, rootIndex+1, eInorder);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
     }
 }
