22d6c98db580deee0b72305c64468c4293049a9b
liangyu <yuliang.name@gmail.com>
2017-01-06 23:10:56 -0500

    sorted list/arry to balanced bst

--- tree/contruct_tree/109_sortedlist_to_balanced_bst/Solution.java
+++ tree/contruct_tree/109_sortedlist_to_balanced_bst/Solution.java
@@ -1,30 +1,34 @@
 /*
-Convert Sorted List to Binary Search Tree:
+109 Convert Sorted List to Binary Search Tree:
 https://leetcode.com/problems/convert-sorted-list-to-binary-search-tree/
 
 Given a singly linked list where elements are sorted in
 ascending order, convert it to a height balanced BST.
 */
 
-
 /**
  * Definition for singly-linked list.
- * struct ListNode {
+ * public class ListNode {
  *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
  */
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
+ * public class TreeNode {
  *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
  */
- 
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        
+    }
+}
+
 public class Solution {
     public TreeNode sortedListToBST(ListNode head) {
         int length = 0;
