fa046674a069eb71dbc7ee62feb122ab132b93c8
liangyu <yuliang.name@gmail.com>
2017-01-07 22:58:06 -0500

    updated tree questions

--- tree/height_size/104_maximum_depth_of_bt/Solution.java
+++ tree/height_size/104_maximum_depth_of_bt/Solution.java
@@ -11,15 +11,24 @@ down to the farthest leaf node.
 
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
  */
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
 public class Solution {
     public int maxDepth(TreeNode root) {
-        
+        if (root == null) {
+        	return 0;
+        } else {
+        	return max(maxDepth(root->left), maxDepth(roo->right)) + 1;
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 }
